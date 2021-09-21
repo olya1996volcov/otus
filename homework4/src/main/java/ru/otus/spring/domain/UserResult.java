d7add787bc19362b30be962ce6dc957ac7d1a5c4
ovolkovnichenko <ovolkovnichenko@diasoft.ru>
2021-09-21 22:48:23 +0300

    fix after review

--- homework4/src/main/java/ru/otus/spring/domain/UserResult.java
+++ homework4/src/main/java/ru/otus/spring/domain/UserResult.java
@@ -1,17 +1,9 @@
 package ru.otus.spring.domain;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 
-@RequiredArgsConstructor
+@Data
 public class UserResult {
     private final User user;
     private final int result;
-
-    public User getUser() {
-        return user;
-    }
-
-    public int getResult() {
-        return result;
-    }
 }
