3c7191bb3618f9d6cc6502f769a42a9187e21049
ovolkovnichenko <ovolkovnichenko@diasoft.ru>
2022-03-03 19:26:14 +0300

    refactoring

--- diploma/src/main/java/ru/otus/spring/LibraryApp.java
+++ diploma/src/main/java/ru/otus/spring/LibraryApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 public class LibraryApp {
     public static void main(String[] args) {
         SpringApplication.run(LibraryApp.class, args);
